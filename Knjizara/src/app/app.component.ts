4ca155013d4020488173473591fbb033e194c67d
17314FilipNikolic <filnik@elfak.rs>
2021-07-08 16:59:02 +0200

    Dodati podaci

--- Knjizara/src/app/app.component.ts
+++ Knjizara/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from './store/app.state';
+import { loadBooks } from './store/books.actions';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Knjizara';
+
+  constructor(private store: Store<AppState>){}
+
+  ngOnInit() {
+    this.store.dispatch(loadBooks());
+  }
 }
